8c38ee597ac28a3ee37bb21b44839a53abd737cc
Jimmy <jimmyramos879@gmail.com>
2024-06-26 20:21:16 -0500

    Added js and html files

---
+++ app.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+
+// var allows the assigned variable to be redeclared, reassimged, and can be mutated. Where as let allows everything but being able to redeclare it.
+
+//With const, you are not allowed to reassign or redaclare the value within. Just like let, it is limited to a block scope.
+
+//let allows the variable to be reassinged, but you cannot do it with const.
+
+//Hoisting is when the varibles are lifted to the top of the scope in which they are being declared. With var in particular, declarations can be invoked before the code within is run.
